76e6d610e378c0936e638b9fdaedc1544b985686
andyweiss1982 <andyweiss1982@gmail.com>
2021-06-01 16:12:57 -0400

    1 file modified

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import React from 'react';
 import AllItems from './pages/AllItems';
 import SingleItem from './pages/SingleItem';
 
-// all matches must be exact except for named parameters
 export const routes = {
   // path / component mappings
   '/items': AllItems,
